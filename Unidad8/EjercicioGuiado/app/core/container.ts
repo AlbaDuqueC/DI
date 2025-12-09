0eac0532f256862d3d4706a9d716e1fdae97f6cc
AlbaDuqueC <alba.duque@iesnervion.es>
2025-12-09 13:41:03 +0100

    Unidad8 -> Ejercicio Guiado

---
+++ Unidad8/EjercicioGuiado/app/core/container.ts
@@ -0,0 +1,18 @@
+import { Container } from "inversify";
+import "reflect-metadata";
+import { PersonasRepo } from "../data/repositories/PersonaRepo";
+import { PersonasRepo100 } from "../data/repositories/PersonaRepo100";
+import { IPersonaRepo } from "../domain/interfaces/IPersonaRepo";
+import { PeopleListVM } from "../UI/ViewModel/PeopleListVM";
+import { TYPES } from "./types";
+
+
+const container = new Container();
+
+
+// Vinculamos la interfaz con su implementación concreta
+container.bind<PersonasRepo100>(TYPES.IRepositoryPersonas).to(PersonasRepo100);
+container.bind<PeopleListVM>(TYPES.IndexVM).to(PeopleListVM);
+export { container };
+
+
